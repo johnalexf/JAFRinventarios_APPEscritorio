6a9ae9ec171c8fd13f8c29c93d237f6d9d437761
johnalexf <johnalexfr@gmail.com>
2026-07-29 16:09:58 -0500

    feat(controladores/validaciones): crear ValidadorFormulario para gestionar y ejecutar validaciones de campos de todo un formulario

---
+++ src/jafrinventarios/controladores/validaciones/ValidadorFormulario.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jafrinventarios.controladores.validaciones;
+
+import java.util.List;
+import java.util.ArrayList;
+import javax.swing.JLabel;
+import javax.swing.text.JTextComponent;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class ValidadorFormulario {
+    
+    private List<CampoFormulario> camposFormulario;
+
+    public ValidadorFormulario() {
+        camposFormulario = new ArrayList<>();
+    }
+    
+    public void agregarCampo( JTextComponent input, JLabel lblError, TipoDatoFormulario tipoDato, boolean esObligatorio ){
+        camposFormulario.add(new CampoFormulario( input, lblError, tipoDato, esObligatorio));
+    }
+    
+    public boolean validar(){
+        
+        boolean respuesta = true;
+        
+        /*Con el for each hacemos que se validen todos los campos y se muestren
+          los errores correspondientes en el lblError de cada input
+        */
+        for(CampoFormulario campo : camposFormulario){
+            // Cambiamos la respuesta a false si cualquiera de los campos
+            // no es un dato valido
+            if(!campo.validar()){
+                respuesta = false;
+            }
+        
+        }
+        
+        return respuesta;
+    }
+    
+
+}
