adc9ae52c9cdacb50db7ac79934c448051b3fe90
johnalexf <johnalexfr@gmail.com>
2026-07-30 13:10:05 -0500

    feat(vistas/utilidades): crear clase utilitaria para poder mostrar contraseña en componentes Jpasswordfield

---
+++ src/jafrinventarios/vistas/utilidades/MostrarOcultarContrasena.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jafrinventarios.vistas.utilidades;
+
+import javax.swing.AbstractButton;
+import javax.swing.ImageIcon;
+import javax.swing.JPasswordField;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public final class MostrarOcultarContrasena {
+    
+    private final static String RUTA_ICONOS = "/jafrinventarios/recursos/iconos/botones/";
+    
+    private static final ImageIcon ICONO_MOSTRAR =
+        new ImageIcon(
+                MostrarOcultarContrasena.class.getResource(
+                        RUTA_ICONOS + "mdi-light--eye.png")
+        );
+
+    private static final ImageIcon ICONO_OCULTAR =
+        new ImageIcon(
+                MostrarOcultarContrasena.class.getResource(
+                        RUTA_ICONOS + "mdi-light--eye-off.png")
+        );
+
+    //Se pone en privado el constructor para evitar que se creen objetos con esta clase
+    //No se borra el constructor por que java por defecto lo creara publico
+    private MostrarOcultarContrasena() {
+    }
+    
+    
+     /**
+     * Agrega al botón la funcionalidad de mostrar u ocultar la contraseña..
+     * @param inputContrasena campo de contraseña a manipular como muestra su contenido
+     * @param btnMostrarOcultarContrasena boton para manipular al campo contraseña
+     */
+    public static void agregarFuncionalidad( JPasswordField inputContrasena, AbstractButton btnMostrarOcultarContrasena ) {
+
+        // Guardamos el carácter original con que se muestra la contraseña
+        // oculta, que puede ser : (•, *, otros..) depende de version de java
+        char caracterOriginal = inputContrasena.getEchoChar();
+
+        // Estado inicial
+        btnMostrarOcultarContrasena.setIcon(ICONO_MOSTRAR);
+
+        btnMostrarOcultarContrasena.addActionListener(e -> {
+
+            boolean contrasenaOculta = inputContrasena.getEchoChar() == caracterOriginal;
+
+            if (contrasenaOculta) {
+                // Mostrar contraseña
+                inputContrasena.setEchoChar((char) 0);
+                btnMostrarOcultarContrasena.setIcon(ICONO_OCULTAR);
+               
+            } else {
+                // Ocultar contraseña
+                inputContrasena.setEchoChar(caracterOriginal);
+                btnMostrarOcultarContrasena.setIcon(ICONO_MOSTRAR);
+
+            }
+        });
+    }
+
+}
