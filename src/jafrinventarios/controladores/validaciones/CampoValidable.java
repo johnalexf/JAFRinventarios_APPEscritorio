a13310b8228798c21bea2147451849d5d9f6f9c6
johnalexf <johnalexfr@gmail.com>
2026-07-31 11:49:46 -0500

    refactor(controladores/validaciones): crear clase abstracta CampoValidable para centralizar logica visual de errores

---
+++ src/jafrinventarios/controladores/validaciones/CampoValidable.java
@@ -0,0 +1,79 @@
+/*
+Esta clase se encarga de recibir cualquier campo de un formulario
+para aplicarle los estilos y mostrar el mensaje pertinente cuando
+tenga un error, como tal esta destinada para ser heredada y poder
+personalizar la respectiva validacion dependiendo del tipo de campo
+ */
+package jafrinventarios.controladores.validaciones;
+
+import java.awt.Color;
+import javax.swing.JComponent;
+import javax.swing.JLabel;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public abstract class CampoValidable {
+    
+    private final JComponent componente;
+    private final JLabel lblError;
+    private final String ESTILO_NORMAL_INPUT =
+                            " borderColor:#777777; focusedBorderColor:#112355 ";
+
+    
+    public CampoValidable(JComponent componente, JLabel lblError) {
+        
+        this.componente = componente;
+        this.lblError = lblError;
+        
+        // Configuramos el label en rojo para los errores y vacio
+        if (this.lblError != null) {
+            this.lblError.setForeground(new Color(179,38,30));
+            this.lblError.setText(""); 
+        }
+        
+        aplicarEstiloNormalAComponente();
+        
+    }
+    
+    
+    private void aplicarEstiloNormalAComponente(){
+        componente.putClientProperty("JComponent.outline", null); 
+        componente.putClientProperty(
+            "FlatLaf.style",
+            ESTILO_NORMAL_INPUT
+        );
+    }
+    
+    private void aplicarEstiloErrorAComponente(){
+         // Con FlatLaf, esto pinta el borde del input en rojo
+            componente.putClientProperty("JComponent.outline", "error");
+    }
+    
+    
+        protected void mostrarError(String mensaje) {
+        if (lblError != null) {
+            lblError.setText(mensaje);
+        }
+        aplicarEstiloErrorAComponente();
+    }
+
+    
+    protected void limpiarError() {
+        if (lblError != null) {
+            lblError.setText("");
+        }
+        // Limpiamos el borde de FlatLaf
+        aplicarEstiloNormalAComponente();
+    }
+    
+    
+    protected abstract void asignarValidacionEnTiempoReal();
+    
+    
+    protected abstract boolean validar();
+    
+
+}
+
