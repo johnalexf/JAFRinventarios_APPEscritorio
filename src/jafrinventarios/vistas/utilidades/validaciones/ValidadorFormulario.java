ede64debd4a456c5bc5bd4a3b9000ce0d95fee35
johnalexf <johnalexfr@gmail.com>
2026-08-05 09:35:26 -0500

    refactor(vistas/validaciones): reestructurar ValidadorFormulario con diccionario y validar obligatoriedad del name

--- src/jafrinventarios/vistas/utilidades/validaciones/ValidadorFormulario.java
+++ src/jafrinventarios/vistas/utilidades/validaciones/ValidadorFormulario.java
@@ -5,10 +5,10 @@
  */
 package jafrinventarios.vistas.utilidades.validaciones;
 
-import java.util.List;
-import java.util.ArrayList;
 import java.util.HashMap;
+import javax.swing.JComponent;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPasswordField;
 import javax.swing.text.JTextComponent;
 
@@ -18,21 +18,58 @@ import javax.swing.text.JTextComponent;
  */
 public class ValidadorFormulario {
     
-    private List<CampoValidable> listaCamposFormulario;
+    /* Guardamos cada campo del formulario con una clave valor, en donde la clave 
+        sera el name de dicho componente, permitiendo asi mostrar errores como 
+        respuesta de la consulta a la base de datos.
+        Ejemplo Correo no encontrado, contraseña incorrecta, alias repetido, etc.
+    */
+    private final HashMap<String, CampoValidable> listaCamposFormulario;
 
     public ValidadorFormulario() {
-        listaCamposFormulario = new ArrayList<>();
+        listaCamposFormulario = new HashMap<>();
     }
     
     public void agregarCampo( JTextComponent input, JLabel lblError, TipoDatoFormulario tipoDato, boolean esObligatorio ){
-        listaCamposFormulario.add(new CampoFormulario( input, lblError, tipoDato, esObligatorio));
+        
+        // Si falta el nombre, la aplicación se detiene aquí mismo con un error en consola
+        validarNombreEnComponente(input);
+        
+        listaCamposFormulario.put( 
+                input.getName() ,
+                new CampoFormulario( input, lblError, tipoDato, esObligatorio)
+        );
+        
     }
     
     public void agregarCampoConfirmarContrasena( 
                                     JPasswordField inputConfirmarContrasena, 
                                     JPasswordField inputContrasena, 
                                     JLabel lblError ){
-        listaCamposFormulario.add(new CampoConfirmarContrasena(inputConfirmarContrasena,inputContrasena,lblError));
+        // Si falta el nombre, la aplicación se detiene aquí mismo con un error en consola
+        validarNombreEnComponente(inputConfirmarContrasena);
+        
+        listaCamposFormulario.put(
+                inputConfirmarContrasena.getName(),
+                new CampoConfirmarContrasena(inputConfirmarContrasena,inputContrasena,lblError)
+        );
+        
+    }
+    
+    private void validarNombreEnComponente(JComponent componente){
+        // Obtenemos el nombre asignado en las propiedades del diseñador visual
+        String nombre = componente.getName();
+        
+        if (nombre == null || nombre.trim().isEmpty()) {
+            // Esto detiene la ejecución del programa inmediatamente (Fail-Fast)
+            // y te muestra en la consola exactamente qué componente falló.
+            throw new IllegalArgumentException(
+                "\n\n Error en agregar campo a validadorFormulario: "
+                + "\n El componente de tipo " 
+                + componente.getClass().getSimpleName() 
+                + " no tiene un 'name' configurado. "
+                + "\n Es obligatorio asignarlo en las propiedades de la vista. \n"
+            );
+        }
     }
     
     public boolean validar(){
@@ -42,7 +79,7 @@ public class ValidadorFormulario {
         /*Con el for each hacemos que se validen todos los campos y se muestren
           los errores correspondientes en el lblError de cada input
         */
-        for(CampoValidable campo : listaCamposFormulario){
+        for(CampoValidable campo : listaCamposFormulario.values()){
             // Cambiamos la respuesta a false si cualquiera de los campos
             // no es un dato valido
             if(!campo.validar()){
@@ -55,7 +92,7 @@ public class ValidadorFormulario {
     }
     
     public void limpiarErrores(){
-        for(CampoValidable campo : listaCamposFormulario){
+        for(CampoValidable campo : listaCamposFormulario.values()){
             campo.limpiarError();
         }
     }
@@ -67,7 +104,7 @@ public class ValidadorFormulario {
                  unico y representativo en la base de datos o el modelo.
         */
         HashMap<String, String> recolectorDatos = new HashMap<>();
-        listaCamposFormulario.forEach(campo -> {
+        listaCamposFormulario.values().forEach(campo -> {
             recolectorDatos.put(campo.getNameComponente(), campo.getValorComponente());
         });
         
