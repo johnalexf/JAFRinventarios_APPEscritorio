4c1f11bd6a136c018e92c85a7504feca04d52946
johnalexf <johnalexfr@gmail.com>
2026-08-14 11:47:19 -0500

    feat(controladores/utilidades): crear ControladorBusquedaYRegistro y su interfaz para centralizar y delegar eventos de busqueda

---
+++ src/jafrinventarios/controladores/usuarios/ControladorUsuarios.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jafrinventarios.controladores.usuarios;
+
+import jafrinventarios.controladores.utilidades.ControladorBusquedaYRegistro;
+import jafrinventarios.vistas.usuarios.UsuariosPanel;
+import jafrinventarios.controladores.utilidades.AccionesBusquedaYRegistro;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class ControladorUsuarios {
+    
+    private final UsuariosPanel vista;
+
+    public ControladorUsuarios(UsuariosPanel vista) {
+        this.vista = vista;
+        
+        // Instanciamos el sub-controlador pasándole el panel incrustado y definiendo qué hacer
+        new ControladorBusquedaYRegistro(   vista.getPanelBusquedaYRegistro(), 
+                                            asignarAccionesBusquedaYRegistro()
+        );
+    }
+    
+    private AccionesBusquedaYRegistro asignarAccionesBusquedaYRegistro(){
+        return new AccionesBusquedaYRegistro() {
+            
+            @Override
+            public void ejecutarBusqueda(String terminoBusqueda) {
+                System.out.println("Buscando en la BD de Usuarios el término: " + terminoBusqueda);
+                // Aquí va tu lógica para filtrar la tabla de usuarios
+            }
+
+            @Override
+            public void agregarNuevoItem() {
+                System.out.println("Abriendo modal para crear nuevo usuario...");
+                // Aquí llamas a tu método estático crearUsuario()
+            }
+            
+        };
+    }
+}
---
+++ src/jafrinventarios/controladores/utilidades/AccionesBusquedaYRegistro.java
@@ -0,0 +1,24 @@
+/*
+ Esta interfaz fue diseñada para que el controladorBusquedaYRegistro sea quien
+ active los eventos de escucha del boton agregar nuevo item y buscar coincidencias
+ sin necesidad que dentro de dicho controlador se declare la logica de
+ busqueda y agregar, por tanto esta interfaz es el puente que permite que el 
+ controlador principal sea quien asigne el codigo de las funciones segun 
+ corresponda y se las pase por medio de un objeto de esta interfaz.
+ */
+package jafrinventarios.controladores.utilidades;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public interface AccionesBusquedaYRegistro {
+    
+    // Esta codificado en ControladorBusquedaYRegistro para que 
+    // se dispare solo si la búsqueda es válida (no vacía, enter o clic)
+    void ejecutarBusqueda(String terminoBusqueda);
+    
+    // Esta codificado en ControladorBusquedaYRegistro para que
+    // se dispare cuando hacen clic en "Agregar Nuevo"
+    void agregarNuevoItem();
+}
