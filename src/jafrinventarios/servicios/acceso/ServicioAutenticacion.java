07c94fbd7c3215a22945a764241b5f82112e9560
johnalexf <johnalexfr@gmail.com>
2026-08-19 14:50:24 -0500

    feat(servicios/acceso): crear estructura inicial de servicios de acceso y registro

---
+++ src/jafrinventarios/servicios/acceso/ServicioAutenticacion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jafrinventarios.servicios.acceso;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class ServicioAutenticacion {
+    /*
+    Esta clase esta destinada para editar contraseña, recuperarla e iniciar sesion
+    TODO pendiente de su estructuracion interna.
+    */
+}
---
+++ src/jafrinventarios/servicios/acceso/ServicioRegistro.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jafrinventarios.servicios.acceso;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class ServicioRegistro {
+    /*
+    Esta clase funcionara para poder registrar un usuario sin haber iniciado sesion
+    en el caso de que un administrador desee crear un usuario lo puede hacer desde 
+    el servicioUsuarios ya que la creacion cambia puesto que la contraseña se enviara
+    por correo al usuario creado, en cambio desde esta clase el registro pide la clave
+    y un codigo de acceso
+    */
+}
