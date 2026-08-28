37a6921890826cf77b63658082cc1c7741d664ab
johnalexf <johnalexfr@gmail.com>
2026-08-28 13:12:38 -0500

    feat(servicios): implementar clase singleton para la conexion a la base de datos

---
+++ src/jafrinventarios/servicios/Conexion_DB.java
@@ -0,0 +1,44 @@
+
+package jafrinventarios.servicios;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class Conexion_DB {
+    
+    private static final String DB_NAME = "db_app_jafr";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "12345678";
+    
+    private static Connection conexionDB = null;
+    
+    
+    public static Connection getConnection()
+    {
+        if (conexionDB != null) 
+            return conexionDB;
+        
+        setConnection();
+        return conexionDB;
+    }
+
+    private static void setConnection() {
+        try
+        {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexionDB = DriverManager.getConnection( 
+                    "jdbc:mysql://localhost/"+ DB_NAME +"?user="+ USERNAME + "&password=" + PASSWORD
+            );
+            System.out.println("Conexion establecida a la base de datos");
+        }
+        catch(Exception e)
+        {
+            e.printStackTrace();
+        }  
+    }
+
+}
