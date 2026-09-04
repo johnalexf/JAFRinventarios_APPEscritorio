a1c6cc4d98e6861a1864036a9bba679ed4016abe
johnalexf <johnalexfr@gmail.com>
2026-09-04 10:53:08 -0500

    feat(DTOs/compras): crear DTOCompraTabla inmutable para listar historial de compras

---
+++ src/jafrinventarios/DTOs/compras/DTOCompraTabla.java
@@ -0,0 +1,81 @@
+
+package jafrinventarios.DTOs.compras;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class DTOCompraTabla {
+    
+    private final Integer idCompra;
+    private final Date fechaHoraCompra;
+    private final double totalCompra;
+    private final String nombreComercialProveedor;
+    private final String aliasUsuario;
+    
+    private final ArrayList<DTODetalleCompraTabla> detalles;
+
+    
+    /*
+    ============================================================================
+                                CONSTRUCTOR
+    ============================================================================
+    */
+    
+    public DTOCompraTabla(  Integer idCompra, 
+                            Date fechaHoraCompra,
+                            double totalCompra, 
+                            String nombreComercialProveedor, 
+                            String aliasUsuario, 
+                            ArrayList<DTODetalleCompraTabla> detalles) {
+        this.idCompra = idCompra;
+        this.fechaHoraCompra = fechaHoraCompra;
+        this.totalCompra = totalCompra;
+        this.nombreComercialProveedor = nombreComercialProveedor;
+        this.aliasUsuario = aliasUsuario;
+        this.detalles = new ArrayList<>(detalles);
+    }
+
+    
+    /*
+    ============================================================================
+                                  GETTERS
+    ============================================================================
+    */
+    
+    public Integer getIdCompra() {
+        return idCompra;
+    }
+
+    public Date getFechaHoraCompra() {
+        return fechaHoraCompra;
+    }
+
+    public double getTotalCompra() {
+        return totalCompra;
+    }
+
+    public String getNombreComercialProveedor() {
+        return nombreComercialProveedor;
+    }
+
+    public String getAliasUsuario() {
+        return aliasUsuario;
+    }
+
+    public ArrayList<DTODetalleCompraTabla> getDetalles() {
+        return new ArrayList<>(detalles);
+    }
+          
+    
+    /*
+    Este modelo no necesita setters puesta esta destinado exclusivamente 
+    para obtener la informacion de forma que sea consistente a lo que espera 
+    ver el usuario, para editar una compra se usara como tal ModeloCompra
+    */
+    
+    
+}
