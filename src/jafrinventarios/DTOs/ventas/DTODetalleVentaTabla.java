352697764dc51c9e7256fdb959dd3a57f08abb10
johnalexf <johnalexfr@gmail.com>
2026-09-07 11:03:31 -0500

    feat(DTOs/ventas): crear DTODetalleVentaTabla inmutable para presentar registros en la interfaz

---
+++ src/jafrinventarios/DTOs/ventas/DTODetalleVentaTabla.java
@@ -0,0 +1,56 @@
+
+package jafrinventarios.DTOs.ventas;
+
+/**
+ *
+ * @author JOHN FORERO
+ */
+public class DTODetalleVentaTabla {
+    
+    private final String nombreProducto;
+    private final int cantidadProducto;
+    private final double precioUnitarioProducto;
+    private final double precioTotalProducto;
+    
+    /*
+    ============================================================================
+                                CONSTRUCTOR
+    ============================================================================
+    */
+
+    public DTODetalleVentaTabla(String nombreProducto, int cantidadProducto, double precioUnitarioProducto, double precioTotalProducto) {
+        this.nombreProducto = nombreProducto;
+        this.cantidadProducto = cantidadProducto;
+        this.precioUnitarioProducto = precioUnitarioProducto;
+        this.precioTotalProducto = precioTotalProducto;
+    }
+    
+    /*
+    ============================================================================
+                                  GETTERS
+    ============================================================================
+    */
+
+    public String getNombreProducto() {
+        return nombreProducto;
+    }
+
+    public int getCantidadProducto() {
+        return cantidadProducto;
+    }
+
+    public double getPrecioUnitarioProducto() {
+        return precioUnitarioProducto;
+    }
+
+    public double getPrecioTotalProducto() {
+        return precioTotalProducto;
+    }
+    
+    /*
+    Este modelo no necesita setters puesta esta destinado exclusivamente 
+    para obtener la informacion de forma que sea consistente a lo que espera 
+    ver el usuario, para editar un detalle se usara como tal ModeloDetallleVenta
+    */     
+
+}
