9d2303bf19f03385e66c22901741f097bc520764
johnalexf <johnalexfr@gmail.com>
2026-09-25 12:55:38 -0500

    feat(controladores/reportes): poblar listas de seleccion independientes y enlazar eventos de los checkboxes

--- src/jafrinventarios/controladores/reportes/ControladorReportes.java
+++ src/jafrinventarios/controladores/reportes/ControladorReportes.java
@@ -7,6 +7,7 @@ import jafrinventarios.servicios.proveedores.ServicioProveedores;
 import jafrinventarios.servicios.usuarios.ServicioUsuarios;
 import jafrinventarios.vistas.reportes.ReportePanel;
 import jafrinventarios.vistas.reportes.ReportePanel.TipoReporteEspecial;
+import java.awt.event.ItemEvent;
 import java.util.LinkedHashMap;
 
 /**
@@ -26,7 +27,17 @@ public class ControladorReportes {
 */
     public ControladorReportes(ReportePanel panelReportes) {
         this.panelReportes = panelReportes;
+        /*
+        NOTA: 
+        Se inicializa los eventos para la seccion del filtro de reportes
+        para ventas y compras, con esto se garantiza que una unica vez se va 
+        realizar la inicializacion y que esta no se ejecute cada vez que el 
+        usuario salga y entre a la seleccion de filtros.
+        */
         inicializarBotonesPrincipales();
+        poblarComboBoxIndependientes();
+        inicializarCheckBoxs();
+        
     }
     
 /*
@@ -58,7 +69,7 @@ public class ControladorReportes {
     
 /*
 ============================================================================
-                    INICIALIZAR EVENTOS BOTONES PRINCIPALES
+                  INICIALIZAR EVENTOS BOTONES PRINCIPALES
 ============================================================================
 */
     private void inicializarBotonesPrincipales(){
@@ -78,8 +89,70 @@ public class ControladorReportes {
         panelReportes.getBtnLinkCancelar().addActionListener( e -> { 
             panelReportes.mostrarPanelConfiguracionReporte(false);
         });
+        
+    }
+    
+    
+/*
+============================================================================
+    Poblar de la lista pertinente para los combo box independientes
+    es decir todos menos productos.
+============================================================================
+*/
+    private void poblarComboBoxIndependientes(){
+    
+        try {
+            panelReportes.setListaProveedores( obtenerDiccionarioProveedores() );
+            panelReportes.setListaClientes( obtenerDiccionarioClientes() );
+            panelReportes.setListaUsuarios( obtenerDiccionarioUsuarios() );
+        } catch (Exception e) {
+            panelReportes.mostrarAlertaError(e.getMessage());
+        }
+    
     }
 
+    
+/*
+============================================================================
+                  INICIALIZAR EVENTOS CHECK BOXS
+============================================================================
+*/
+    public void inicializarCheckBoxs(){
+    
+        panelReportes.getCheckBoxProveedores().addItemListener( (e) -> {
+            if( e.getStateChange() == ItemEvent.SELECTED ){
+                panelReportes.habilitarProveedores(true);
+            }else if( e.getStateChange() == ItemEvent.DESELECTED  ){
+                panelReportes.habilitarProveedores(false);
+            }
+        });
+        
+        panelReportes.getCheckBoxClientes().addItemListener((e) -> {
+            if( e.getStateChange() == ItemEvent.SELECTED ){
+                panelReportes.habilitarClientes(true);
+            }else if( e.getStateChange() == ItemEvent.DESELECTED  ){
+                panelReportes.habilitarClientes(false);
+            }
+        });
+        
+        panelReportes.getCheckBoxProductos().addItemListener((e) -> {
+            if( e.getStateChange() == ItemEvent.SELECTED ){
+                panelReportes.habilitarProductos(true);
+            }else if( e.getStateChange() == ItemEvent.DESELECTED  ){
+                panelReportes.habilitarProductos(false);
+            }
+        });
+        
+        panelReportes.getCheckBoxUsuarios().addItemListener((e) -> {
+            if( e.getStateChange() == ItemEvent.SELECTED ){
+                panelReportes.habilitarUsuarios(true);
+            }else if( e.getStateChange() == ItemEvent.DESELECTED  ){
+                panelReportes.habilitarUsuarios(false);
+            }
+        });
+        
+    }
+    
 /*
 ============================================================================
         METODOS PARA LAS ACCIONES (CREAR REPORTE DEPENDIENDO DEL TIPO)
@@ -91,7 +164,20 @@ public class ControladorReportes {
     
     private void mostrarFiltroReporte( TipoReporteEspecial tipoReporte ){
         this.tipoReporte = tipoReporte;
+        switch(tipoReporte){
+            case ReporteCompras:
+                
+                break;
+            case ReporteVentas:
+                try {
+                    panelReportes.setListaProductos( obtenerDiccionarioProductos() );
+                } catch (Exception e) {
+                    panelReportes.mostrarAlertaError( e.getMessage() );
+                }
+                break;
+        }
         panelReportes.mostrarConfiguracionReporte( tipoReporte );
+        
     }
     
     
