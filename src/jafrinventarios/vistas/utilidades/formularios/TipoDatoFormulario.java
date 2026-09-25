a0386d2ae34b65977a277bcadfbe9fec4966a719
johnalexf <johnalexfr@gmail.com>
2026-09-25 08:01:48 -0500

    feat(vistas/utilidades): crear CampoFecha y agregar validacion regex para fechas sin hora

--- src/jafrinventarios/vistas/utilidades/formularios/TipoDatoFormulario.java
+++ src/jafrinventarios/vistas/utilidades/formularios/TipoDatoFormulario.java
@@ -31,6 +31,7 @@ public enum TipoDatoFormulario {
     NUMERO_ENTERO_MAYOR_CERO("^[1-9][0-9]*$", "Número invalido"),
     NUMERO_DOUBLE("^[0-9]+(?:\\.[0-9]{1,2})?$", "Solo reales de hasta 2 decimales"),
     FECHA_HORA("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}\\s(0[1-9]|1[0-2]):[0-5][0-9]\\s(?i)(a\\.?\\s?m\\.?|p\\.?\\s?m\\.?)$", "Debe ser dd/mm/aaaa hh:mm a. m. "),
+    FECHA("^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\\d{4}$", "Debe ser dd/mm/aaaa "),
     /*
     REQUERIDO es para aquellos campos que por lo menos deben tener algun caracter
     ya que su contenido no depende de una validacion en la vista, si no que de una
