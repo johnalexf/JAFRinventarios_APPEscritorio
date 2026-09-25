b641b4946a29a7e9c632a336bebbcaf0a4d43625
johnalexf <johnalexfr@gmail.com>
2026-09-25 11:30:02 -0500

    feat(controladores/reportes): agregar metodos envoltorio para consultar diccionarios de datos a los servicios

--- src/jafrinventarios/controladores/reportes/ControladorReportes.java
+++ src/jafrinventarios/controladores/reportes/ControladorReportes.java
@@ -1,8 +1,13 @@
 
 package jafrinventarios.controladores.reportes;
 
+import jafrinventarios.servicios.clientes.ServicioClientes;
+import jafrinventarios.servicios.productos.ServicioProductos;
+import jafrinventarios.servicios.proveedores.ServicioProveedores;
+import jafrinventarios.servicios.usuarios.ServicioUsuarios;
 import jafrinventarios.vistas.reportes.ReportePanel;
 import jafrinventarios.vistas.reportes.ReportePanel.TipoReporteEspecial;
+import java.util.LinkedHashMap;
 
 /**
  *
@@ -23,6 +28,33 @@ public class ControladorReportes {
         this.panelReportes = panelReportes;
         inicializarBotonesPrincipales();
     }
+    
+/*
+============================================================================
+            METODOS PARA CONSULTAR A LOS SERVICIOS
+============================================================================
+*/
+    
+    private LinkedHashMap<Integer, String> obtenerDiccionarioProveedores() throws Exception{
+        return ServicioProveedores.obtenerDiccionarioProveedores( false );
+    }
+    
+    private LinkedHashMap<Integer, String> obtenerDiccionarioClientes() throws Exception{
+        return ServicioClientes.obtenerDiccionarioClientes( false );
+    }
+    
+    private LinkedHashMap<Integer, String> obtenerDiccionarioProductosPorProveedor( int idProveedor ) throws Exception{
+        return ServicioProductos.obtenerDiccionarioProductosPorProveedor(idProveedor);
+    }
+    
+    private LinkedHashMap<Integer, String> obtenerDiccionarioProductos() throws Exception{
+        return ServicioProductos.obtenerDiccionarioProductos();
+    }
+    
+    private LinkedHashMap<Integer, String> obtenerDiccionarioUsuarios() throws Exception{
+        return ServicioUsuarios.obtenerDiccionarioUsuarios();
+    }
+    
     
 /*
 ============================================================================
